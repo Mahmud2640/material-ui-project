1a2743769f932a7d21e0fb7e94dbe4a9ef240006
Mahmud2640 <mdmahmud484@gmail.com>
2023-01-23 00:34:45 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Components/Layout/Layout";
-import Home from "./Pages/Home/Home";
+import Router from "./Routes/Router";
 import { theme } from "./Theme/theme";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [{ element: <Home />, path: "/home" }],
-  },
-]);
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <Router />
       <CssBaseline />
     </ThemeProvider>
   );
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Box, Container } from "@mui/material";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 function Layout() {
   return (
-    <div>Layout</div>
-  )
+    <Box>
+      <Container>
+        <Header />
+        <Outlet />
+      </Container>
+    </Box>
+  );
 }
 
-export default Layout
+export default Layout;
