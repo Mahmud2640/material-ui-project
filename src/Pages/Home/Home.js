6aab05778faeed3c77b3035498e26bfdd81cc87a
Mahmud2640 <mdmahmud484@gmail.com>
2023-01-25 22:34:47 +0600

    brann

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Branding from "./Branding/Branding";
+import Brand from "./Brands/Brand";
+import Faq from "./Faq/Faq";
 
 function Home() {
   return (
     <div>
       <Branding />
+      <Brand/>
+      <Faq/>
     </div>
   );
 }
---
+++ src/Pages/Home/OurWorks/Ourworks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ourworks = () => {
+     return (
+          <div>
+               
+          </div>
+     );
+};
+
+export default Ourworks;
