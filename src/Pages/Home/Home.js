eab82ea0954798860d0ff1a4340606b13dbb01ac
Mahmud2640 <mdmahmud484@gmail.com>
2023-01-28 19:26:46 +0600

    our works

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Branding from "./Branding/Branding";
 import Brand from "./Brands/Brand";
+import Ourworks from "./OurWorks/Ourworks";
 import Services from "./Service/Services";
 
 function Home() {
   return (
     <div>
       <Branding />
-      <Brand/>
-      <Services/>
+      <Brand />
+      <Services />
+      <Ourworks />
     </div>
   );
 }
--- src/Pages/Home/OurWorks/Ourworks.js
+++ src/Pages/Home/OurWorks/Ourworks.js
@@ -1,11 +1,88 @@
-import React from 'react';
+import { Box, IconButton, Tab, Tabs } from "@mui/material";
+import React from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const Ourworks = () => {
-     return (
-          <div>
-               
-          </div>
-     );
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box sx={{ my: 5 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <SectionTitle
+          title="Here are some of"
+          colored="Our works"
+          sx={{
+            textAlign: "left",
+          }}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: 2,
+          }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            centered
+            sx={{
+              "& .MuiTabs-indicator": {
+                display: "none",
+              },
+              "& .MuiButtonBase-root": {
+                textTransform: "capitalize",
+              },
+              "& .Mui-selected": {
+                color: "primary.green",
+                fontWeight: "bold",
+              },
+            }}
+          >
+            <Tab label="All" />
+            <Tab label="Interior" />
+            <Tab label="Exterior" />
+            <Tab label="Building" />
+          </Tabs>
+          <Box>
+            <IconButton
+              sx={{
+                border: "1px solid #959EAD",
+                mr: 1,
+              }}
+              onClick={() => setValue(value - 1)}
+              disabled={value === 0}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            <IconButton
+              sx={{
+                border: "1px solid #959EAD",
+              }}
+              onClick={() => setValue(value + 1)}
+              disabled={value === 3}
+            >
+              <ArrowBackIcon
+                sx={{
+                  transform: "rotate(180deg)",
+                }}
+              />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Ourworks;
